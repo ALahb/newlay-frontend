b7dd797e2be87b4c60774170da75d436e073e6af
Rayen Ben Saad <rayen.bensaad@onetech-group.com>
2025-07-20 10:50:49 +0100

    fix

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://api.radgate.rology.net/api',
+    baseURL: 'https://api.radgate.rology.net/api',
     headers: {
         'Content-Type': 'application/json',
         'Cache-Control': 'no-cache',
